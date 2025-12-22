f7665243a991680bebd0369841c20dfcf9f25b05
Loadofspaghetti <max.jaderstrom@outlook.com>
2025-12-22 01:08:43 +0100

    Fully functioning sender 1, basically implemented everything, but note that i use absolute imports (use: python -m sender.sender_v1)

--- senders/sender_v1.py
+++ senders/sender_v1.py
@@ -10,13 +10,40 @@ from utils.global_definitions import (
     width, height
 )
 
-start_time = 0
-encode_time = 0
-end_time = 0
+start_time = 0.5
+bit_time = 0.3
+end_time = 0.5
+
+timer = 0
 
 message = "HELLO"
 
 frame = None
+
+
+def bit_frames(bgr, duration, width=width, height=height):
+    """
+    Gets the bgr and then keeps the frame the same for a period of time
+    
+    Arguments:
+        bgr (tuple): An array to create the image with
+        width (int): An int for the width
+        height (int): An int for the height
+
+    Returns:
+        None
+    """
+
+    timer = time.time()
+    while time.time() - timer < duration:
+
+        frame = create_frame_bgr(bgr, width, height)
+        cv2.imshow(window, frame)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            cv2.destroyAllWindows()
+            return
+        
 
 def sender():
     """
@@ -30,8 +57,45 @@ def sender():
         None
     """
 
-    start_time = time.time()
-    while time.time() - start_time < 0.5:
+
+    # A green start frame to signal the beginning of the sender
+
+    timer = time.time()
+    while time.time() - timer < start_time:
+
+        frame = create_frame_bgr(green_bgr, width, height)
+        cv2.imshow(window, frame)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            cv2.destroyAllWindows()
+            return
+        
+    
+    # The actual bits
+        
+    for character in message:
+
+        bits = format(ord(character), "08b")
+
+        for bit in bits:
+            
+            # The actual bits
+            if bit == "1":
+                bit_frames(white_bgr, bit_time)
+            else:
+                bit_frames(black_bgr, bit_time)
+            
+            # Sync frame in between each bit
+            bit_frames(blue_bgr, bit_time)
+        
+        # Red frame to indicate the end of the character
+        bit_frames(red_bgr, bit_time)
+
+    
+    # A green frame at the end to signal the end of sender
+    
+    timer = time.time()
+    while time.time() - timer < end_time:
 
         frame = create_frame_bgr(green_bgr, width, height)
         cv2.imshow(window, frame)
